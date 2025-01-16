565450f33d5a4e599462db731d5047556b70c7c6
Sam Rohrbach <sam.g.rohrbach@proton.me>
2025-01-16 11:02:12 -0700

    IT FINALLY WORKS!!!!!

--- nextlaunch.py
+++ nextlaunch.py
@@ -11,17 +11,16 @@ import json
 import os
 from datetime import *
 
-badVal = "{'name':'no','net':'whenever'}"
-tmpFile = '/tmp/nextlaunch.json'
+badVal = "{'name':'no','net':'whenever'}" # something is better than nothing
+tmpFile = '/tmp/nextlaunch.json' # change to whatever windows temp dir if on windows
 url = "https://ll.thespacedevs.com/2.3.0/launches/upcoming/?limit=1&mode=list&offset=2" # launchlibrary2 2.3, only the next launch in list
-manifest = ""
 
 def signal_handler(sig, frame): # handle termination well
-  logger.info("Received signal to stop, exiting")
-  sys.stdout("Received signal to stop, exiting")
-  sys.stdout.write("\n")
-  sys.stdout.flush()
-  sys.exit(0)
+	logger.info("Received signal to stop, exiting")
+	sys.stdout("Received signal to stop, exiting")
+	sys.stdout.write("\n")
+	sys.stdout.flush()
+	sys.exit(0)
 
 # signal handlers for termination
 signal.signal(signal.SIGINT, signal_handler)
@@ -29,74 +28,66 @@ signal.signal(signal.SIGTERM, signal_handler)
 signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
 def fetchJSON(url): # get json from url
-    response = requests.get(url)
-    response.raise_for_status()  # raise an HTTPError on a bad status
-    return response.json()
+	response = requests.get(url)
+	response.raise_for_status() # raise an HTTPError on a bad status
+	return response.json()
 
 def saveJSON(data, tmpFile): # save to file
-    with open(tmpFile, 'w') as file:
-        json.dump(data, file, indent=4)
+	with open(tmpFile, 'w') as file:
+		json.dump(data, file, indent=4)
 
 def whenLastModified(tmpFile): # find when file last modified
-    timestamp = os.path.getmtime(tmpFile)
-    return datetime.fromtimestamp(timestamp)
-
-def getJSONValue(data, key): # get value from json
-	data2 = json.loads(data)
-	return data2[key]
+	timestamp = os.path.getmtime(tmpFile)
+	return datetime.fromtimestamp(timestamp)
 
 def main():
-	global manifest
+	manifest = "" # make sure it exists first
 	# does file exist?
 	try:
 		if os.path.exists(tmpFile):
-			print(f"File already exists")
-		else:
-			fileThing = open(tmpFile, "w")
-			fileThing.write(badVal)
-			print(f"Created file at {tmpFile} with contents {badVal}")
-			with open(tmpFile, "r") as fileThing:
-				manifest = fileThing.read()
-	except FileNotFoundError:
-		fileThing = open(tmpFile, "w")
-		fileThing.write(badVal)
-		print(f"Created file at {tmpFile} with contents {badVal}")
-		with open(tmpFile, "r") as fileThing:
-			manifest = fileThing.read()
-	
-	print(f"manifest is {manifest}")
-
-	# when file last modified?
-	try:
-		if os.path.exists(tmpFile):
+			#print(f"File already exists at {tmpFile}")
 			lastModifiedDate = whenLastModified(tmpFile)
 			timeDifference = datetime.now() - lastModifiedDate
-			if timeDifference < timedelta(minutes=10) and manifest != badVal:
-				#return "File modified less than 10 minutes ago"
-				print(f"File was modified less than 10 minutes ago at {lastModifiedDate}. Not overwriting.")
+			if manifest == badVal or manifest == "":
+				jsonData = fetchJSON(url)
+				saveJSON(jsonData, tmpFile)
+				#print(f"JSON data saved to {tmpFile}")
+				#print(f"Last modified: {whenLastModified(tmpFile)}")		
+			if (timeDifference < timedelta(minutes=10)):
+				print(f"Cache was modified recently at {lastModifiedDate}, skipping overwrite")
 			else:
 				jsonData = fetchJSON(url)
 				saveJSON(jsonData, tmpFile)
-				print(f"JSON data saved to {tmpFile}")
-				print(f"Last modified: {whenLastModified(tmpFile)}")
+				#print(f"JSON data saved to {tmpFile}")
+				#print(f"Last modified: {whenLastModified(tmpFile)}")
 		else:
+			fileThing = open(tmpFile, "w")
+			fileThing.write(badVal)
+			print(f"Cache does not exist at {tmpFile}, creating one")
+			with open(tmpFile, "r") as fileThing:
+				manifest = fileThing.read()
 			jsonData = fetchJSON(url)
 			saveJSON(jsonData, tmpFile)
-			print(f"JSON data saved to {tmpFile}")
-			print(f"Last modified: {whenLastModified(tmpFile)}")
-	except TypeError: #Exception as e:
-		print(f"kndskhdsfjhfdjkd")
-		#print(f"An error occurred: {e}")
+			#print(f"JSON data saved to {tmpFile}")
+			#print(f"Last modified: {whenLastModified(tmpFile)}")
+	except FileNotFoundError:
+		fileThing = open(tmpFile, "w")
+		fileThing.write(badVal)
+		print(f"Cache does not exist at {tmpFile}, creating one")
+		with open(tmpFile, "r") as fileThing:
+			manifest = fileThing.read()
+		jsonData = fetchJSON(url)
+		saveJSON(jsonData, tmpFile)
+		#print(f"JSON data saved to {tmpFile}")
 	
-	# get actual data
-	mission = getJSONValue(manifest, 'name')
-	net = getJSONValue(manifest, 'net')
-	print(mission + net)
-	if mission == "Key not found" or net == "Key not found":
-		print(f":(")
-		print(f"Your computer didn't run into a problem and doesn't need to restart. We didn't collect anything and we won't restart for you. ")
-		print(f"Stop code: ")
-		return ":("
-	print(f"Next launch: {mission} at {net}")		
+	with open(tmpFile, "r") as fileThing:
+		manifest = fileThing.read()
 
-main() # FINALLY!!!!!1
+	# get actual data
+	wholeThing = json.loads(manifest)
+	mission = wholeThing["results"][0]["name"]
+	net = wholeThing["results"][0]["net"]
+	return(f"Next launch: {str(mission)} at {str(net)}")
+	#return results
+
+print(f"{main()}") # FINALLY!!!!!1
