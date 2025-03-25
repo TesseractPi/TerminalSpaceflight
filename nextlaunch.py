f10faa37e9ce3ab8bc85d4cf1c33754ed6ae7e10
TesseractPi <sam.g.rohrbach@proton.me>
2025-03-25 08:19:09 -0700

    move temp to ~/.terminalspaceflightwhatever

--- nextlaunch.py
+++ nextlaunch.py
@@ -16,7 +16,7 @@ from datetime import *
 runningOn = platform.system()
 fileName = ".terminalSpaceflightTempFile.json"
 badVal = "{'name':'no','net':'whenever'}" # something is better than nothing
-tmpFileLinux = "/etc/" # or whatever
+tmpFileLinux = "~/" # or whatever
 tmpFileWindows = "%TEMP%\\"
 tmpFileMac = "$tmpFile/"
 url = "https://ll.thespacedevs.com/2.3.0/launches/upcoming/?limit=1&mode=list&offset=1" # launchlibrary2 2.3, only the next launch in list
